00d1f52dff015a01115ebab4820028da3f361d2a
amiroslaw <arek47@gmail.com>
2021-10-18 20:07:12 +0200

    remove toc plug

--- scully.miroslaw.config.ts
+++ scully.miroslaw.config.ts
@@ -1,5 +1,4 @@
 import {ScullyConfig, setPluginConfig} from '@scullyio/scully';
-import {getTocPlugin, TocConfig} from 'scully-plugin-toc';
 import {getHttp404Plugin} from '@gammastream/scully-plugin-http404';
 
 import './scully/plugins/emoji.js';
@@ -10,21 +9,10 @@ require('@notiz/scully-plugin-medium-zoom');
 // 'lazyImages',
 const Http404Plugin = getHttp404Plugin();
 const defaultPostRenderers = [
-  'toc',
   'emoji',
   // 'mediumZoom',
   Http404Plugin
 ];
-
-const tocOptions: TocConfig = {
-  insertSelector: '#toc',
-  level: ['h2', 'h3'],
-  trailingSlash: true,
-  scrollIntoViewOnClick: true
-};
-const TocPlugin = getTocPlugin();
-
-setPluginConfig(TocPlugin, tocOptions);
 
 export const config: ScullyConfig = {
   projectRoot: './src',

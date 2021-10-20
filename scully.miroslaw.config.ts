36f464c4639d4c9da7dd71f8a152e22248cfdccd
amiroslaw <arek47@gmail.com>
2021-10-20 22:03:13 +0200

    add scully toc plugin

--- scully.miroslaw.config.ts
+++ scully.miroslaw.config.ts
@@ -2,15 +2,29 @@ import {ScullyConfig, setPluginConfig} from '@scullyio/scully';
 import {getHttp404Plugin} from '@gammastream/scully-plugin-http404';
 // import '@notiz/scully-plugin-rss';
 
+import {getTocPlugin, TocConfig} from 'scully-plugin-toc';
+
 import './scully/plugins/emoji.js';
 import './scully/plugins/tags.js';
 
 const Http404Plugin = getHttp404Plugin();
 const defaultPostRenderers = [
   'seoHrefOptimise',
+  'toc',
   'emoji',
   Http404Plugin
 ];
+
+
+const tocOptions: TocConfig = {
+  insertSelector: '#toc',
+  level: ['h2', 'h3'],
+  trailingSlash: true,
+  scrollIntoViewOnClick: true
+};
+const TocPlugin = getTocPlugin();
+
+setPluginConfig(TocPlugin, tocOptions);
 
 export const config: ScullyConfig = {
   projectRoot: './src',

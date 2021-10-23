09d11590b9f3315b36ff4e0b3b468f7ac3afb182
amiroslaw <arek47@gmail.com>
2021-10-23 03:06:08 +0200

    add time to read

--- scully/plugins/article.ts
+++ scully/plugins/article.ts
@@ -1,7 +1,16 @@
 import {JSDOM} from 'jsdom';
-import {HandledRoute, registerPlugin} from '@scullyio/scully';
+import {HandledRoute, logError, registerPlugin} from '@scullyio/scully';
+
+function getWordcount(body: HTMLBodyElement) {
+  let content = body.textContent;
+  content = content.replace(/(^\s*)|(\s*$)/gi, '');
+  content = content.replace(/[ ]{2,}/gi, ' ');
+  content = content.replace(/\n /, '\n');
+  return content.split(' ').length;
+}
 
 function toc(dom: JSDOM) {
+
   const tocLinks = dom.window.document.querySelectorAll('#toc a');
   if (tocLinks.length > 0) {
     for (let i = 0; i < tocLinks.length; i++) {
@@ -12,8 +21,25 @@ function toc(dom: JSDOM) {
   }
 }
 
+function addReadingTime(dom: JSDOM) {
+  let body = dom.window.document.querySelector('body');
+  if (body) {
+    const wordcount = getWordcount(body);
+    // const readingTimeMin = Math.floor(wordcount/160);
+    // const readingTimeSec = (wordcount/160 - readingTimeMin) * 60;
+    const readingTimeMin = Math.round(wordcount / 160);
+    const post = dom.window.document.querySelector('article.post');
+    post.insertAdjacentHTML('afterbegin', `<div id="reading-time"> ${readingTimeMin} min  </div>`);
+  }
+}
+
 const articlePlugin = async (dom: JSDOM, routeData: HandledRoute): Promise<string> => {
   toc(dom);
+  const isArticle = routeData.route.includes('/blog/');
+  if (isArticle) {
+    addReadingTime(dom);
+  }
+
   return dom;
 };
 

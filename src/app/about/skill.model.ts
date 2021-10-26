9dd6fa2fb6c00e1c89db9598f1239855ec17d046
amiroslaw <arek47@gmail.com>
2021-10-26 02:38:40 +0200

    portfolio changes

--- src/app/about/skill.model.ts
+++ src/app/about/skill.model.ts
@@ -3,11 +3,11 @@ export class Skill {
   }
 
   static skills: Array<Skill> = [
-    new Skill('Java', 'Android, Spring Boot, JavaFX', 75, '#27ae60', '#2ecc71'),
-    new Skill('SQL', 'Hibernate, SQLite, ORMLite, MySql', 50, '#2980b9', '#3498db'),
-    new Skill('Testing', 'jUnit, Mockito', 30, '#46465e', '#5a68a5'),
-    new Skill('HTML5 CSS3', 'Bootstrap 4', 55, '#d35400', '#e67e22'),
-    new Skill('Java Script', 'jQuery', 30, '#FFA000', '#FFC107'),
+    new Skill('Java', 'Liferay, Spring Boot, JSP, JavaFX, Android ', 80, '#27ae60', '#2ecc71'),
+    new Skill('SQL', 'Hibernate, SQLite, ORMLite, MySql', 60, '#2980b9', '#3498db'),
+    new Skill('Testing', 'jUnit, Selenium, Mockito', 40, '#46465e', '#5a68a5'),
+    new Skill('HTML5 CSS3', 'Bootstrap 5, FreeMarker', 65, '#d35400', '#e67e22'),
+    new Skill('JavaScript', 'jQuery, Angular, TS', 35, '#FFA000', '#FFC107'),
     new Skill('Construct', 'Construct', 60, '#7B1FA2', '#9C27B0')
   ];
 

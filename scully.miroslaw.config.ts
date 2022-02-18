9b87fa1db0a7c8819c41d8981e289e1d75ff2ff9
amiroslaw <arek47@gmail.com>
2022-02-18 22:23:51 +0100

    change rss plugin in order to work with asciidoc files

--- scully.miroslaw.config.ts
+++ scully.miroslaw.config.ts
@@ -1,9 +1,8 @@
 import {ScullyConfig, setPluginConfig} from '@scullyio/scully';
 import {getHttp404Plugin} from '@gammastream/scully-plugin-http404';
-import '@notiz/scully-plugin-rss';
 import {timeToRead, timeToReadOptions} from 'scully-plugin-time-to-read';
-import scully_plugin_page_title from 'scully-plugin-page-title';
 
+import './scully/plugins/rss.js';
 import './scully/plugins/emoji.js';
 import './scully/plugins/tags.js';
 import './scully/plugins/article.js';
@@ -14,7 +13,6 @@ setPluginConfig(timeToRead, {
 
 const Http404Plugin = getHttp404Plugin();
 const defaultPostRenderers = [
-  'changeTitlePlugin',
   'seoHrefOptimise',
   'emoji',
   'article',
---
+++ scully/plugins/rss.js
@@ -0,0 +1,126 @@
+const Feed = require('feed').Feed;
+const { registerPlugin } = require('@scullyio/scully');
+const { writeFileSync, readFileSync } = require('fs');
+const { join } = require('path');
+const { log, logError, yellow } = require('@scullyio/scully');
+const asciidoctor = require('asciidoctor.js')()
+
+const configFile = readFileSync(`${process.cwd()}/rss.config.json`, 'utf8');
+const config = JSON.parse(configFile.toString());
+const blogPostRouteSlug = config.blogPostRouteSlug || '/blog';
+const filename = config.filename || 'feed';
+const feed = new Feed(config);
+
+config.categories.forEach((cat) => {
+  feed.addCategory(cat);
+});
+
+const rssPlugin = (routes) => {
+  log('Started @notiz/scully-plugin-rss');
+
+  const blogPosts = routes.filter(
+    (r) =>
+      r && r.data && r.data.published && r.route.includes(blogPostRouteSlug)
+  );
+
+  if (config.newestPostsFirst) {
+    blogPosts.sort((a, b) => {
+      return a.data.publishedAt > b.data.publishedAt ? -1 : 1;
+    });
+  } else {
+    blogPosts.sort((a, b) => {
+      return a.data.publishedAt > b.data.publishedAt ? 1 : -1;
+    });
+  }
+
+  log(
+    `Generating RSS Feed for ${yellow(blogPosts.length)} published blog ${
+      blogPosts.length === 1 ? 'post' : 'posts'
+    }`
+  );
+
+  blogPosts.forEach((r) => {
+    const item = createFeedItemFromRoute(r);
+    feed.addItem(item);
+  });
+  try {
+    writeFileSync(join(config.outDir || '', `${filename}.xml`), feed.rss2());
+    log(`✅ Created ${yellow(config.outDir + `/${filename}.xml`)}`);
+    writeFileSync(join(config.outDir || '', `${filename}.atom`), feed.atom1());
+    log(`✅ Created ${yellow(config.outDir + `/${filename}.atom`)}`);
+    writeFileSync(join(config.outDir || '', `${filename}.json`), feed.json1());
+    log(`✅ Created ${yellow(config.outDir + `/${filename}.json`)}`);
+  } catch (error) {
+    logError('❌ Failed to create RSS feed. Error:', error);
+    throw error;
+  }
+  log('Finished @notiz/scully-plugin-rss');
+};
+
+const createFeedItemFromRoute = (route) => {
+  let item;
+  try {
+    if (route.data.published) {
+      const mdString = readFileSync(route.templateFile, 'utf8').toString();
+
+      const asciidoc = mdString.slice(
+        nth_occurrence(mdString, '---', 2) + 3,
+        mdString.length - 1
+      );
+      const articleHTML = asciidoctor.convert(asciidoc)
+
+      item = {
+        title: route.data.title,
+        id: route.route,
+        link: config.link + route.route,
+        description: route.data.description,
+        content: articleHTML,
+        author: route.data.authors
+          ? route.data.authors.map((a) => ({ name: a }))
+          : [],
+        contributor: route.data.authors
+          ? route.data.authors.map((a) => ({
+              name: a.toLowerCase().replace(' ', '-'),
+            }))
+          : [],
+        date: route.data.updatedAt
+          ? route.data.updatedAt
+          : route.data.publishedAt,
+        image: route.data.twitterBanner,
+      };
+    }
+  } catch (err) {
+    logError(`Error during feed item creation ${route.data.route}`, err);
+  }
+
+  return item;
+};
+
+function nth_occurrence(text, searchString, nth) {
+  const firstIndex = text.indexOf(searchString);
+  const lengthUpToFirstIndex = firstIndex + 1;
+
+  if (nth === 1) {
+    return firstIndex;
+  } else {
+    const stringAfterFirstOccurrence = text.slice(lengthUpToFirstIndex);
+    const nextOccurrence = nth_occurrence(
+      stringAfterFirstOccurrence,
+      searchString,
+      nth - 1
+    );
+
+    if (nextOccurrence === -1) {
+      return -1;
+    } else {
+      return lengthUpToFirstIndex + nextOccurrence;
+    }
+  }
+}
+
+module.exports = {
+  rssPlugin,
+};
+
+const validator = async () => [];
+registerPlugin('routeDiscoveryDone', 'rss', rssPlugin, validator);
